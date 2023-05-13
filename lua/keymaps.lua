0f7789ed120c817a005b0225aeea0ebf99391027
t-akira012 <aki012.tech@gmail.com>
2023-05-13 14:53:41 +0900

    [MOD] gj, gk の keymap を kemayps.lua に移動

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,9 +3,15 @@ local h = require('helper')
 
 vim.g.mapleader = " "
 
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
 h.nmap(";", ":")
-h.nmap("j", "gj")
-h.nmap("k", "gk")
 h.imap("jj", "<ESC>")
 
 h.nmap("<C-b>", ":Ex<CR>")
