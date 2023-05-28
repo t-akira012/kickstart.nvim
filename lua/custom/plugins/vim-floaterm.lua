912572cd7ce316a52e24f5d05ca1284959685523
t-akira012 <aki012.tech@gmail.com>
2023-05-28 23:07:28 +0900

    [MOD] W3mGoogle コマンドを追加

--- lua/custom/plugins/vim-floaterm.lua
+++ lua/custom/plugins/vim-floaterm.lua
@@ -3,15 +3,19 @@ return {
 	config = function()
 		vim.keymap.set('n', ',,', '<CMD>FloatermToggle<CR>', { silent = true })
 		vim.keymap.set('v', ',,', '<CMD>FloatermToggle<CR>', { silent = true })
-		vim.keymap.set('t', '<ESC>', '<CMD>FloatermToggle<CR>', { silent = true })
-		-- vim.keymap.set('n', '<C-l>', '<CMD>FloatermToggle<CR>', { silent = true })
-		-- vim.keymap.set('v', '<C-l>', '<CMD>FloatermToggle<CR>', { silent = true })
-		-- vim.keymap.set('t', '<C-l>', '<CMD>FloatermToggle<CR>', { silent = true })
+		vim.keymap.set('t', '<ESC>', '<CMD>FloatermHide<CR>', { silent = true })
+		vim.keymap.set('t', '<C-g>', '<CMD>FloatermShow google<CR>', { silent = true })
+		vim.keymap.set('t', '<C-c>', '<CMD>FloatermKill google<CR>', { silent = true })
 		local function floaterm_toggle()
 			-- TODO: タブラベルと紐つける
 			vim.api.nvim_command('FloatermToggle tab' .. vim.fn.tabpagenr())
 		end
 
+		vim.api.nvim_create_user_command('W3mGoogle',
+			'FloatermNew --name=google --autoclose=1 w3m google.com/search\\?q=<q-args>',
+			{ nargs = 1 })
+
+		vim.keymap.set('n', '<C-g>', ':W3mGoogle ', { silent = false })
 		vim.keymap.set('n', '<C-l>', floaterm_toggle, { silent = true })
 		vim.keymap.set('t', '<C-l>', floaterm_toggle, { silent = true })
 		vim.keymap.set('t', '<C-l>', '<CMD>FloatermHide<CR>', { silent = true })
