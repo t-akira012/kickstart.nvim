1b12f89a15d97ba1e2287f89e12a2191a8e5a16d
t-akira012 <aki012.tech@gmail.com>
2023-05-28 22:32:21 +0900

    [MOD] タブごとに floaterm を起動するようにする

--- lua/custom/plugins/vim-floaterm.lua
+++ lua/custom/plugins/vim-floaterm.lua
@@ -4,6 +4,18 @@ return {
 		vim.keymap.set('n', ',,', '<CMD>FloatermToggle<CR>', { silent = true })
 		vim.keymap.set('v', ',,', '<CMD>FloatermToggle<CR>', { silent = true })
 		vim.keymap.set('t', '<ESC>', '<CMD>FloatermToggle<CR>', { silent = true })
+		-- vim.keymap.set('n', '<C-l>', '<CMD>FloatermToggle<CR>', { silent = true })
+		-- vim.keymap.set('v', '<C-l>', '<CMD>FloatermToggle<CR>', { silent = true })
+		-- vim.keymap.set('t', '<C-l>', '<CMD>FloatermToggle<CR>', { silent = true })
+		local function floaterm_toggle()
+			-- TODO: タブラベルと紐つける
+			vim.api.nvim_command('FloatermToggle tab' .. vim.fn.tabpagenr())
+		end
+
+		vim.keymap.set('n', '<C-l>', floaterm_toggle, { silent = true })
+		vim.keymap.set('t', '<C-l>', floaterm_toggle, { silent = true })
+		vim.keymap.set('t', '<C-l>', '<CMD>FloatermHide<CR>', { silent = true })
+
 		vim.cmd([[
 			augroup vimrc_floaterm
 				autocmd!
