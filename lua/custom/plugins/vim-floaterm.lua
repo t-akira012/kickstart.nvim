83c8138c51c669892ebc5edb7369ea45bd063979
t-akira012 <aki003xyz@gmail.com>
2023-07-27 12:54:01 +0900

    vim-floaterm の keybind を C-l に変更

--- lua/custom/plugins/vim-floaterm.lua
+++ lua/custom/plugins/vim-floaterm.lua
@@ -1,17 +1,17 @@
 return {
-	"voldikss/vim-floaterm",
-	config = function()
-		vim.keymap.set('n', '<C-q>', '<CMD>FloatermToggle new<CR>', { silent = true })
-		vim.keymap.set('t', '<C-q>', '<CMD>FloatermHide new<CR>', { silent = true })
+    "voldikss/vim-floaterm",
+    config = function()
+        vim.keymap.set('n', '<C-l>', '<CMD>FloatermToggle new<CR>', { silent = true })
+        vim.keymap.set('t', '<C-l>', '<CMD>FloatermHide new<CR>', { silent = true })
 
-		vim.keymap.set('n', '<F2>', '<CMD>FloatermToggle google<CR>', { silent = true })
-		vim.keymap.set('t', '<F2>', '<CMD>FloatermToggle google<CR>', { silent = true })
-		vim.keymap.set('n', '<C-g>', ':W3mGoogle ', { silent = false })
-		vim.api.nvim_create_user_command('W3mGoogle',
-			'FloatermNew --name=google --height=0.6 --autoclose=1 w3m google.com/search\\?q=<q-args>',
-			{ nargs = 1 })
+        vim.keymap.set('n', '<F2>', '<CMD>FloatermToggle google<CR>', { silent = true })
+        vim.keymap.set('t', '<F2>', '<CMD>FloatermToggle google<CR>', { silent = true })
+        vim.keymap.set('n', '<C-g>', ':W3mGoogle ', { silent = false })
+        vim.api.nvim_create_user_command('W3mGoogle',
+            'FloatermNew --name=google --height=0.6 --autoclose=1 w3m google.com/search\\?q=<q-args>',
+            { nargs = 1 })
 
-		vim.cmd([[
+        vim.cmd([[
 			let g:floaterm_width=0.99
 			let g:floaterm_height=0.8
 			hi FloatermBorder guibg=none guifg=cyan
@@ -19,6 +19,6 @@ return {
 				autocmd!
 				autocmd QuitPre * FloatermKill!
 			augroup END
-		]])
-	end
+		]]     )
+    end
 }
