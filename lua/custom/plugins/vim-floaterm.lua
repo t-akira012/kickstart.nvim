3c431871c1a4f161e214c6ce92817a486a05eafa
t-akira012 <aki003xyz@gmail.com>
2024-01-16 13:58:57 +0900

    feat: 🎸 floatermの表示位置を修正、telescope-file-browserを使うのをやめる

--- lua/custom/plugins/vim-floaterm.lua
+++ lua/custom/plugins/vim-floaterm.lua
@@ -1,24 +1,19 @@
 return {
     "voldikss/vim-floaterm",
     config = function()
-        vim.keymap.set('n', '<C-l>', '<CMD>FloatermToggle new<CR>', { silent = true })
-        vim.keymap.set('t', '<C-l>', '<CMD>FloatermHide new<CR>', { silent = true })
-
-        vim.keymap.set('n', '<F2>', '<CMD>FloatermToggle google<CR>', { silent = true })
-        vim.keymap.set('t', '<F2>', '<CMD>FloatermToggle google<CR>', { silent = true })
-        vim.keymap.set('n', '<C-g>', ':W3mGoogle ', { silent = false })
-        vim.api.nvim_create_user_command('W3mGoogle',
-            'FloatermNew --name=google --height=0.6 --autoclose=1 w3m google.com/search\\?q=<q-args>',
-            { nargs = 1 })
+        vim.keymap.set('n', '<C-j>', '<CMD>FloatermToggle new<CR>', { silent = true })
+        vim.keymap.set('t', '<C-j>', '<CMD>FloatermHide new<CR>', { silent = true })
 
         vim.cmd([[
-			let g:floaterm_width=0.99
-			let g:floaterm_height=0.8
+			let g:floaterm_wintype='split'
+			let g:floaterm_position='rightbelow'
+			let g:floaterm_width=1
+			let g:floaterm_height=0.4
 			hi FloatermBorder guibg=none guifg=cyan
 			augroup vimrc_floaterm
 				autocmd!
 				autocmd QuitPre * FloatermKill!
 			augroup END
-		]]     )
+		]])
     end
 }
