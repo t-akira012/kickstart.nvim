0bcf3f5d30acf731efe3a24a5c612e069e665810
t-akira012 <aki003xyz@gmail.com>
2024-01-16 16:02:09 +0900

    feat: 🎸 terminalをfloatermからtoggletermに変更

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,16 @@
+return {
+    "akinsho/toggleterm.nvim",
+    config = function()
+        require("toggleterm").setup({
+            open_mapping = [[<c-j>]],
+            size = function(term)
+                if term.direction == "horizontal" then
+                    return 20
+                elseif term.direction == "vertical" then
+                    return vim.o.columns * 0.4
+                end
+            end,
+            autochdir = true,
+        })
+    end
+}
