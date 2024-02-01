c105a20d922d007405bdb29bd669a2a11ef2eb4b
tamura-dxe-lab <tamura-akira@dxe-lab.com>
2024-02-01 20:57:51 +0900

    refactor: 💡 update plugins

--- lua/custom/plugins/telescope-file-browser.lua
+++ lua/custom/plugins/telescope-file-browser.lua
@@ -1,32 +1,32 @@
 return {
-    -- 'nvim-telescope/telescope-file-browser.nvim',
-    -- dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-    -- config = function()
-    --     -- You don't need to set any of these options.
-    --     -- IMPORTANT!: this is only a showcase of how you can set default options!
-    --     require("telescope").setup {
-    --         extensions = {
-    --             file_browser = {
-    --                 theme = "ivy",
-    --                 -- disables netrw and use telescope-file-browser in its place
-    --                 hijack_netrw = false,
-    --                 hidden = true,
-    --                 respect_gitignore = false,
-    --                 mappings = {
-    --                     ["i"] = {
-    --                     },
-    --                     ["n"] = {
-    --                     },
-    --                 },
-    --             },
-    --         },
-    --     }
-    --     vim.api.nvim_set_keymap(
-    --         "n",
-    --         "<Leader>o",
-    --         ":Telescope file_browser<CR>",
-    --         { noremap = true }
-    --     )
-    --     require("telescope").load_extension "file_browser"
-    -- end
+    'nvim-telescope/telescope-file-browser.nvim',
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+        -- You don't need to set any of these options.
+        -- IMPORTANT!: this is only a showcase of how you can set default options!
+        require("telescope").setup {
+            extensions = {
+                file_browser = {
+                    theme = "ivy",
+                    -- disables netrw and use telescope-file-browser in its place
+                    hijack_netrw = false,
+                    hidden = true,
+                    respect_gitignore = false,
+                    mappings = {
+                        ["i"] = {
+                        },
+                        ["n"] = {
+                        },
+                    },
+                },
+            },
+        }
+        vim.api.nvim_set_keymap(
+            "n",
+            "<Leader>o",
+            ":Telescope file_browser<CR>",
+            { noremap = true }
+        )
+        require("telescope").load_extension "file_browser"
+    end
 }
