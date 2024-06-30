dcd5920808f79271979e074a629351da4b7d5ac4
t-akira012 <aki003xyz@gmail.com>
2024-06-30 23:44:06 +0900

    disable shfmt

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -1,34 +1,34 @@
 return {
-    'stevearc/conform.nvim',
-    config = function()
-        require('conform').setup {
-            formatters_by_ft = {
-                lua = { 'stylua' },
-                python = { 'ruff_format' },
-                javascript = { 'deno_fmt' },
-                typescript = { 'deno_fmt' },
-                javascriptreact = { 'deno_fmt' },
-                typescriptreact = { 'deno_fmt' },
-                sql = { 'sqlfmt' },
-                sh = { 'shfmt' },
-                go = { 'gofmt' },
-            },
-            format_on_save = {
-                lsp_fallback = true,
-                timeout_ms = 500,
-            },
+  'stevearc/conform.nvim',
+  config = function()
+    require('conform').setup {
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        python = { 'ruff_format' },
+        javascript = { 'deno_fmt' },
+        typescript = { 'deno_fmt' },
+        javascriptreact = { 'deno_fmt' },
+        typescriptreact = { 'deno_fmt' },
+        sql = { 'sqlfmt' },
+        -- sh = { 'shfmt' },
+        go = { 'gofmt' },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        timeout_ms = 500,
+      },
+    }
+    require('conform').formatters.deno_fmt = {
+      -- https://github.com/stevearc/conform.nvim/blob/5541c54cf2ab078a537838e1fb9d96ae47f71255/lua/conform/formatters/deno_fmt.lua#L13
+      inherit = false,
+      command = 'deno',
+      args = function(self, ctx)
+        return {
+          'fmt',
+          '--options-no-semicolons',
+          '-',
         }
-        require('conform').formatters.deno_fmt = {
-            -- https://github.com/stevearc/conform.nvim/blob/5541c54cf2ab078a537838e1fb9d96ae47f71255/lua/conform/formatters/deno_fmt.lua#L13
-            inherit = false,
-            command = 'deno',
-            args = function(self, ctx)
-                return {
-                    'fmt',
-                    '--options-no-semicolons',
-                    '-',
-                }
-            end,
-        }
-    end,
+      end,
+    }
+  end,
 }
--- lua/custom/plugins/hop.lua
+++ lua/custom/plugins/hop.lua
@@ -1,7 +1,7 @@
 return {
-	'phaazon/hop.nvim',
-	config = function()
-		require('hop').setup({ keys = 'qwerasdfyuiophjkl' })
-		vim.keymap.set('n', '<leader><leader>', '<CMD>HopChar2<CR>', { silent = true })
-	end
+  'phaazon/hop.nvim',
+  config = function()
+    require('hop').setup { keys = 'qwerasdfyuiophjkl' }
+    vim.keymap.set('n', '<leader><leader>', '<CMD>HopWord<CR>', { silent = true })
+  end,
 }
