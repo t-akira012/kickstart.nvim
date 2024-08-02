64dc00db1d6f6a5e4fa10e3294ca1ee016fe45e8
t-akira012 <aki003xyz@gmail.com>
2024-08-02 13:36:31 +0900

    update

--- lua/custom/plugins/memolist.lua
+++ lua/custom/plugins/memolist.lua
@@ -2,7 +2,7 @@ return {
   'glidenote/memolist.vim',
   config = function()
     vim.cmd [[
-        nnoremap -m  :MemoNew<CR>
+        nnoremap mn  :MemoNew<CR>
         let g:memolist_path = expand('$HOME') .."/docs/doc/mmmm"
         " suffix type (default markdown)
         let g:memolist_memo_suffix = "md"
--- lua/custom/plugins/telescope-memo.lua
+++ lua/custom/plugins/telescope-memo.lua
@@ -1,8 +1,8 @@
 return {
   'delphinus/telescope-memo.nvim',
   config = function()
-    vim.keymap.set('n', '--', '<CMD>Telescope memo list<CR>')
-    vim.keymap.set('n', '-g', '<CMD>Telescope memo live_grep<CR>')
+    vim.keymap.set('n', 'mm', '<CMD>Telescope memo list<CR>')
+    vim.keymap.set('n', 'mg', '<CMD>Telescope memo live_grep<CR>')
     pcall(require('telescope').load_extension, 'memo')
   end,
 }
