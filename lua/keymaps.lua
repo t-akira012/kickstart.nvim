d5a7044ff89a40ee4d4c0ab22fc8f8dc992332a5
t-akira012 <aki003xyz@gmail.com>
2025-05-08 11:06:14 +0900

    update

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -67,6 +67,7 @@ vim.cmd [[
   " add current date
   inoreabbrev ddb <C-R>=substitute(system("date -v-1d '+\%Y-\%m-\%d (\%a)'"), '\n', '', 'g')<CR>
   inoreabbrev <expr> aaa strftime('# %Y-%m-%d (%a) %H:%M')
+  inoreabbrev <expr> xxx strftime('==★== %H:%M')
   inoreabbrev <expr> ddd strftime('%Y-%m-%d (%a)')
   inoreabbrev dda <C-R>=substitute(system("date -v+1d '+\%Y-\%m-\%d (\%a)'"), '\n', '', 'g')<CR>
   inoreabbrev <expr> lll strftime('%-m/%d %a')
