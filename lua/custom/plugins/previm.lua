169c7095aef1f72e6a1310e90f0b425616440a62
t-akira012 <aki003xyz@gmail.com>
2025-06-24 18:33:56 +0900

    update

--- lua/custom/plugins/previm.lua
+++ lua/custom/plugins/previm.lua
@@ -3,6 +3,9 @@ return {
   config = function()
     vim.cmd [[
 			let g:previm_open_cmd = "~/bin/_open_browser"
-		]]
+			let g:previm_disable_default_css = 1
+			let g:previm_custom_css_path = '~/.config/nvim/assets/previm.css'
+			let g:previm_hard_line_break = 1
+	    ]]
   end,
 }
