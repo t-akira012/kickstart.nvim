693ad342763ed4847a4dea8618436f0170111597
t-akira012 <aki003xyz@gmail.com>
2025-09-18 14:56:36 +0900

    add neovide config

--- init.lua
+++ init.lua
@@ -623,3 +623,19 @@ cmp.setup {
     },
   },
 }
+
+if vim.g.neovide then
+  vim.fn.chdir(vim.fn.expand '~/docs/doc')
+
+  -- Neovide-specific settings
+  vim.g.neovide_position_animation_length = 0
+  vim.g.neovide_cursor_animation_length = 0.00
+  vim.g.neovide_cursor_trail_size = 0
+  vim.g.neovide_cursor_animate_in_insert_mode = false
+  vim.g.neovide_cursor_animate_command_line = false
+  vim.g.neovide_scroll_animation_far_lines = 0
+  vim.g.neovide_scroll_animation_length = 0.00
+
+  -- Neovide-specific colorscheme (optional)
+  vim.cmd 'colorscheme habamax'
+end
